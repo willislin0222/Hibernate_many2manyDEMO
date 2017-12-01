67a177f212d3f33e28862e18d2e80c726f023f47
DESKTOP-0U5VJ0C\willis <love0725@gmail.com>
2017-12-01 13:23:00 +0800

    調整成購物車樣式 新增一筆訂單會自動新增訂單明細

--- src/com/user1/model/User1DAO.java
+++ src/com/user1/model/User1DAO.java
@@ -1,6 +1,8 @@
 package com.user1.model;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -13,43 +15,42 @@ public class User1DAO {
 
 	public static void main(String[] args) {
 		
+		List<ServerVO> serverlist = new LinkedList<ServerVO>();
 		ServerVO server1 = new ServerVO();
+		server1.setServerid(19);
 		server1.setAddress("PC-219"); 
 		server1.setUsers(new HashSet());
 		
 		ServerVO server2 = new ServerVO(); 
+		server2.setServerid(18);
 		server2.setAddress("PC-220"); 
 		server2.setUsers(new HashSet());
 		
 		ServerVO server3 = new ServerVO(); 
+		server3.setServerid(20);
 		server3.setAddress("PC-221");
 		server3.setUsers(new HashSet());
+		
+		serverlist.add(server1);
+		serverlist.add(server2);
+		serverlist.add(server3);
+		
+		System.out.println(serverlist.size());
+		
 		
 		User1VO user1 = new User1VO(); 
 		user1.setName("caterpillar");
 		user1.setServers(new HashSet());
-		
-		User1VO user2 = new User1VO(); 
-		user2.setName("momor");
-		user2.setServers(new HashSet());
-		
+			
 		// 多對多，互相參考
-		user1.getServers().add(server1); 
-		user1.getServers().add(server2); 
-		user1.getServers().add(server3); 
-		server1.getUsers().add(user1); 
-		server2.getUsers().add(user1); 
-		server3.getUsers().add(user1); 
-		        
-		user2.getServers().add(server1); 
-		user2.getServers().add(server3); 
-		server1.getUsers().add(user2); 
-		server3.getUsers().add(user2); 
-		        
+		for(int i=0 ; i<serverlist.size() ; i++){
+			user1.getServers().add(serverlist.get(i));
+			serverlist.get(i).getUsers().add(user1);
+		}
+		        	        
 		Session session = HibernateUtil.getSessionFactory().openSession(); 
 		Transaction tx= session.beginTransaction(); 
-		session.save(user1); 
-		session.save(user2); 
+		session.saveOrUpdate(user1);
 		        
 		tx.commit();
 		session.close();
